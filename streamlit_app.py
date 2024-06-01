fbb5851d9e41777f5f8de9c41a5711fba5b3cf90
readerSpace <115855396+readerSpace@users.noreply.github.com>
2024-06-01 13:58:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,19 @@
-x = 10
-'x: ', x 
+df = pd.read_csv('./genre_corr.csv')
+
+with open("genre_dataset_for_pandas.csv", encoding="utf-8") as f:
+    data = list(csv.reader(f))
+
+df_1 = df.query('var1 == "女装"')
+# df_1
+df_2 = df.query('var2 == "女装"')
+# df_2
+df_name = pd.concat(
+    [df_1, df_2],
+    axis=0,
+    ignore_index=True
+)
+# df_3
+df_name = df_name.query('-0.001 <= corr <= 0.001')
+df_name = df_name.sort_values('corr')
+df_name = df_name[::-1]
+df_name[:30]
